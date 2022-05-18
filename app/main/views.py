5527ac9249b44f2cee542917a10fa8d14be4ae73
Kisavi <deniskisavi@gmail.com>
2022-05-18 13:33:24 +0300

    optimize the signup page

--- app/main/views.py
+++ app/main/views.py
@@ -5,3 +5,15 @@ from flask import render_template, url_for, redirect, request
 @main.route('/')
 def index():
     return render_template('landing-page.html')
+
+@main.route('/login')
+def login():
+    return render_template('login.html')    
+
+@main.route('/signup')
+def signup():
+    return render_template('sign-up.html')    
+
+@main.route('/review')
+def review():
+    return render_template('comments.html')
